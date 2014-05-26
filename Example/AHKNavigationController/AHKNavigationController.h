2de188f963e6d9524811b7fac1adbc5be50a1cd2
Arkadiusz Holko <fastred@fastred.org>
2014-05-26 11:22:27 +0200

    Use Podfile in the example project

--- Example/AHKNavigationController/AHKNavigationController.h
+++
@@ -1,8 +0,0 @@
-#import <UIKit/UIKit.h>
-
-@interface AHKNavigationController : UINavigationController
-<UINavigationControllerDelegate, UIGestureRecognizerDelegate>
-
-@property (nonatomic, getter = isPushingViewController) BOOL pushingViewController;
-
-@end
