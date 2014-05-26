05b8aea4f40a6c8947c83b0d90d4bfc513a46d5a
Arkadiusz Holko <fastred@fastred.org>
2014-05-26 12:17:40 +0200

    Add a comment to AHKNavigationController’s header file

--- Classes/AHKNavigationController.h
+++ Classes/AHKNavigationController.h
@@ -2,6 +2,7 @@
 
 #import <UIKit/UIKit.h>
 
+/// A UINavigationController subclass allowing the interactive pop gesture when the navigation bar is hidden or a custom back button is used.
 @interface AHKNavigationController : UINavigationController
 
 @end
